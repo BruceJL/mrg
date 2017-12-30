8d35665fe2df562bdf6f901b3ca49387d9b79d2a
Bruce Link <bruce@bruce486DX.1045.ca>
2017-12-30 17:19:24 -0600

    Removed dasherizer hack. Seems to have fixed the pluralization issue.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import pluralize from 'ember-inflector';
 import ENV from 'mrg-sign-in/config/environment';
 
 var hostValue = '';
-var nameSpace = 'mrg';
+var nameSpace = 'api/mrg';
 if (ENV.environment === 'development') {
 	hostValue = 'http://registration:5000';
 	//hostValue = 'http://localhost:1337';
@@ -13,17 +14,6 @@ if (ENV.environment === 'development') {
 export default DS.RESTAdapter.extend({
 	namespace: nameSpace,
 	host: hostValue,
-
-	//So ember is currently changing the model name of ring-assignment
-	//to ringAssignments instead of ring-assignments.
-	//This would appear to be fixed as per 
-	//https://github.com/emberjs/data/issues/2318 by I'm seeing otherwise
-	//so I've implimented the fix listed in
-	//https://github.com/ember-cli/ember-cli/issues/2906
-	pathForType: function(type) {
-    	var dasherized = Ember.String.dasherize(type);
-    	return Ember.String.pluralize(dasherized);
-  	},
 
 	shouldReloadAll(store, snapshotsArray) {
     	return true;
