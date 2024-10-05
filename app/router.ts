69d7eeeda380cdd95053b68364086595b985248a
Bruce Link <Bruce@1045.ca>
2024-10-05 14:36:50 -0500

    eslint prettier update

--- app/router.ts
+++ app/router.ts
@@ -1,39 +1,38 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'mrg-sign-in/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
+Router.map(function () {
   this.route('login');
 
-  this.route('robots', function() {
+  this.route('robots', function () {
     this.route('edit', {
-      path: '/:robot_id'
+      path: '/:robot_id',
     });
     this.route('new');
     this.route('bulk-payment');
   });
 
-  this.route('competitions', function() {
+  this.route('competitions', function () {
     this.route('show', {
-      path: '/:competition_id'
+      path: '/:competition_id',
     });
     this.route('admin', {
-      path: '/:competition_id/admin'
+      path: '/:competition_id/admin',
     });
   });
 
   this.route('ring-assignments', {
-    path: 'ring-assignments/:competition_id'
+    path: 'ring-assignments/:competition_id',
   });
 
   this.route('checkin', {
-    path: 'checkin/:competition_id'
+    path: 'checkin/:competition_id',
   });
-
 
   this.route('robot');
   this.route('log');
