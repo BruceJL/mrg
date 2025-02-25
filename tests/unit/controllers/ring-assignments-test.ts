dd732d165d04985cb0c723c7a35250d621f535b3
Tracy Huang <tmacmm@gmail.com>
2025-02-25 13:38:03 -0600

    fix/clean up some mismatch test files

--- tests/unit/controllers/ring-assignments-test.ts
+++ tests/unit/controllers/ring-assignments-test.ts
@@ -6,7 +6,7 @@ module('Unit | Controller | ring assignments', function(hooks) {
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:ringAssignment');
+    let controller = this.owner.lookup('controller:ringAssignments');
     assert.ok(controller);
   });
 });
